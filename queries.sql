a6a9d44abdde335da26a967839d6a1ee4784320d
Patrick Plaisted <patrick.plaisted@gmail.com>
2020-10-14 19:00:36 -0700

    initial commit

---
+++ queries.sql
@@ -0,0 +1,12 @@
+create table so_qa (
+    questioner_id int,
+    question_asked_date text,
+    question_score int,
+    question_title text,
+    question_text text,
+    id int,
+    answerer_id int,
+    question_answered_date text,
+    answer_score int,
+    answer_text text
+);
